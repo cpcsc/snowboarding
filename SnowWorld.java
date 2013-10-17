cf31516588ff1e00456e2671ef860f6d42b2e610
jeffy888 <jeffreyxie@pcp079866pcs.wireless.calpoly.edu>
2013-10-17 10:37:04 -0700

    Music is now in sounds

--- SnowWorld.java
+++ SnowWorld.java
@@ -8,9 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SnowWorld extends World
 {
-
-    private Lives theLives;
     private GreenfootSound bkgMusic;
+    private Lives theLives;
+   
     public SnowWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
