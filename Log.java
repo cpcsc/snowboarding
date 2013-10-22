30ba5d37b098fad26f6c54f0de864caa28ee809d
Ricky-Escobar <esco.ricky@gmail.com>
2013-10-22 10:05:50 -0700

    x

---
+++ Log.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Log here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Log extends Obstacles
+{
+    /**
+     * Act - do whatever the Log wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        logMove();
+    }    
+    public void logMove() {
+        setLocation(getX(), getY()+3);
+        if (atWorldBottom()){
+            getWorld().removeObject(this);
+        }
+    }
+}
